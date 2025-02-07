9d1786cc00d4e6b93fead59eecbbe43a143536f8
Ltaudato1 <gitta.tawer13@gmail.com>
2025-02-07 19:34:24 +0300

    solve case a != 0

---
+++ lab1/quadratic_solver.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef QUADRATIC_SOLVER_HPP
+#define QUADRATIC_SOLVER_HPP
+
+void solveQuadraticEquation(double const a, double const b, double const c, double *x1, double *x2);
+
+#endif
